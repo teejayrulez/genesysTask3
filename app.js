41ecefa486dbafd17c43164685cbdaab6ad4ae0e
Tochukwu Mezue <teejaymezue8@gmail.com>
2024-01-19 23:13:52 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+//Product Data
+const products = [
+  { id: 1, name: "Alienware Laptop", price: 1500 },
+  { id: 2, name: "Asus Laptop", price: 1300 },
+  { id: 3, name: "HP Laptop", price: 1200 },
+  { id: 4, name: "Lenovo Laptop", price: 1000 },
+  { id: 5, name: "Dell Laptop", price: 1100 },
+  { id: 6, name: "Desktop", price: 900 },
+];
+
+// Display product
+function displayProducts() {
+  console.log("Available Products:");
+  console.log("--------------------");
+  products.forEach((products) => {
+    console.log(`${products.id}. ${products.name} - $${products.price}`);
+  });
+  console.log("--------------------");
+};
+
+//Add product to cart
+function addToCart(productId, quantity) {
+    const product = products.find(p => p.id === productId);
+    if (product) {
+        cart.push({ ...product, quantity });
+        console.log(`${quantity} ${product.name}(s) added to cart`);
+    } else {
+        console.log('Invalid product ID.');
+    }
+};
+
+//Display shopping cart
+function displayCart() {
+    console.log('Shopping cart:');
+    console.log('--------------------');
+    cart.forEach(item => {
+        console.log(`${item.name} x ${item.quantity} - $${item.price * item.quantity}`);
+    });
+    console.log('--------------------');
+    const total = cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
+    console.log(`Total: $${total}`);
+};
+
+displayCart()
+addToCart()
+displayProducts()
